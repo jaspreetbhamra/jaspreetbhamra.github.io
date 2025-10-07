d9776a73e04302355a3a098a19040fa6b24e6a1a
Jay <17236763+jaspreetbhamra@users.noreply.github.com>
2025-10-07 16:54:49 -0700

    to deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/jaspreetbhamra.github.io/',
   plugins: [react()],
   test: {
     globals: true,
