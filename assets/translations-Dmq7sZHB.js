84347d37fe8ada937eb80639138403fe103864d5
Jay <17236763+jaspreetbhamra@users.noreply.github.com>
2025-10-07 16:54:01 -0700

    Updates

---
+++ assets/translations-Dmq7sZHB.js
@@ -0,0 +1,22 @@
+const e={english:`<This website is still under construction...>
+
+Hello!
+
+🧠 I’m a Data Scientist / ML Engineer with 5+ years of experience wrangling data, building models, and data pipelines
+📊 I love using data to tell stories — the kind that make people go “wait… what?”
+💻 I love programming! There’s just something deeply satisfying about building things from scratch — it feels like digital magic
+🎨 I’m also pretty artsy. I sketch, draw, and paint (sometimes all at once, lol) — my latest side project has been attempting wood carving to carve an Evee, haha
+🎶 Music = life. I sing, play guitar, dabble in Indian classical music, and have recently started learning drums and experimenting with Ableton (aka “me vs Ableton: the saga”)
+
+This site is a mix of all the things I love — science, creativity, curiosity, and a lot of chaos. Welcome to my corner of the internet 🌍
+
+Thank you for stopping by! I hope you find something here that makes you smile, think, or hum a tune, or even better, all three at once.`,elvish:`Hallo!
+
+I’m a Data Sientist / M L Engineer with around five years of experience rangling data, building models, and data pipelines
+I love using data to tell stories — the kind that make people go “wait … what?”
+I love programming! There’s just something deeply satisfying about building things from scratch — it feels like digital magic.
+I’m also pretty artsy. I sketch, draw, and paint (sometimes all at once, lol) — my latest side project has been attempting wood carving to carve an Eevee, haha.
+Music = life. I sing, play guitar, dabble in Indian classical music, and have recently started learning drums and experimenting with A B L E T O N (aka “me vs A B L E T O N: the saga”).
+This site is a mix of all the things I love — science, creativity, curiosity, and a lot of chaos. Welcome to my corner of the internet.
+
+Thank you for stopping by! I hope you find something here that makes you smile, think, or hum a tune, or even better, all three at once.`},t={blurb:e};export{e as blurb,t as default};
