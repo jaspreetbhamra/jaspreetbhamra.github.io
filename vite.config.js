c9fad902016d83ec02c226170eb79a004a10cd20
Jay <17236763+jaspreetbhamra@users.noreply.github.com>
2025-10-30 17:14:30 -0700

    fix: vite config command parameter undefined

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
 	base: command === "serve" ? "/" : "/jaspreetbhamra.github.io/",
-	// base: '/',
 	plugins: [react()],
 	test: {
 		globals: true,
 		environment: "jsdom",
 		setupFiles: "./tests/setup.js",
 	},
-});
+}));
